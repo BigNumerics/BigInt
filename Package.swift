e2a1f8db8964aa20521f7157c13a976f3069e119
David Roman <2538074+davdroman@users.noreply.github.com>
2026-02-04 20:29:09 +0000

    WIP

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
 	platforms: [
 		// required to support StaticBigInt - wish there were a way to fallback to Int
 		.macOS("13.3"), .iOS("16.4"), .tvOS("16.4"), .watchOS("9.4"),
-		.macCatalyst("13.0")
+		.macCatalyst("16.4")
 	],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
